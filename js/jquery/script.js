df74c15c15460f37b7f03bb6d96781119006ca5b
abhinayyar93 <abhishek.nayyar93@gmail.com>
2017-01-30 19:37:54 +0530

    Updated Jumbotron,Footer and Newsletter

--- js/jquery/script.js
+++ js/jquery/script.js
@@ -1,6 +1,6 @@
 $(function(){
 $("#vegas").vegas({
-  delay:4000,
+  delay:5000,
   slides:[
     { src:'./images/BN-1.jpg' },
     { src:'./images/BN-2.jpg' },
@@ -11,17 +11,9 @@ $("#vegas").vegas({
 });
 })
 
+$(function() {
 
-var arr = ['Consistency in performance, greater comfort and safety.',
-           'Designed to replicate the best natural grass fields.',
-           'Delivering a twelve-month season, rain or shine',
-           'Fast pitches, true ball roll and increased player safety.'];
-var index = 1;
-setInterval(function() {
-    document.getElementById("vegas-text").innerHTML = arr[index];
-    // document.getElementById("a1").innerHTML = arr[index];
-    index += 1;
-    if (index == arr.length)
-        index = 0
-    
-}, 5000);
+$('#dg-container').gallery({autoplay	:	true});
+
+});
+
